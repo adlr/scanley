d46a5cd0f75c2ed5e82c568f422890a3a9a6e92e
Andrew de los Reyes <adlr@chromium.org>
2013-04-20 10:11:04 -0700

    added hello worls nacl example. works!

--- knob.js
+++ knob.js
@@ -1,3 +1,67 @@
+HelloTutorialModule = null;  // Global application object.
+statusText = 'NO-STATUS';
+
+// Indicate load success.
+function moduleDidLoad() {
+  HelloTutorialModule = document.getElementById('hello_tutorial');
+  updateStatus('SUCCESS');
+}
+
+// The 'message' event handler.  This handler is fired when the NaCl module
+// posts a message to the browser by calling PPB_Messaging.PostMessage()
+// (in C) or pp::Instance.PostMessage() (in C++).  This implementation
+// simply displays the content of the message in an alert panel.
+function handleMessage(message_event) {
+  alert(message_event.data);
+}
+
+// If the page loads before the Native Client module loads, then set the
+// status message indicating that the module is still loading.  Otherwise,
+// do not change the status message.
+function pageDidLoad() {
+  console.log('adlr loaded');
+  
+  var listener = document.getElementById('listener');
+  listener.addEventListener('load', moduleDidLoad, true);
+  listener.addEventListener('message', handleMessage, true);
+  
+  setupKnob();
+  
+  if (HelloTutorialModule == null) {
+    updateStatus('LOADING...');
+  } else {
+    // It's possible that the Native Client module onload event fired
+    // before the page's onload event.  In this case, the status message
+    // will reflect 'SUCCESS', but won't be displayed.  This call will
+    // display the current message.
+    updateStatus();
+  }
+}
+
+window.addEventListener("load", function load(event){
+    window.removeEventListener("load", load, false); //remove listener, no longer needed
+    pageDidLoad();  
+},false);
+
+
+// Set the global status message.  If the element with id 'statusField'
+// exists, then set its HTML to the status message as well.
+// opt_message The message test.  If this is null or undefined, then
+// attempt to set the element with id 'statusField' to the value of
+// |statusText|.
+function updateStatus(opt_message) {
+  if (opt_message)
+    statusText = opt_message;
+  var statusField = document.getElementById('status_field');
+  if (statusField) {
+    statusField.innerHTML = statusText;
+  }
+}
+
+
+
+function setupKnob() {
+
 var POWERMATE_VENDOR_ID = 1193;//0x077d;
 var POWERMATE_PRODUCT_ID = 8717; //0x0410;
 var DEVICE_INFO = {"vendorId": POWERMATE_VENDOR_ID, "productId": POWERMATE_PRODUCT_ID};
@@ -64,3 +128,4 @@ chrome.permissions.contains( permissionObj, function(result) {
     gotPermission();
   }
 });
+}
